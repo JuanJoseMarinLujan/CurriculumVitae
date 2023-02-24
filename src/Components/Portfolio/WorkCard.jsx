c7dadfd9e3abb84c6412a751a54cdd09efa15d96
Juan Jose Marin Lujan <juanjose.marinlujan@gmail.com>
2023-02-24 17:38:41 -0500

    Update WorkCards with Tools

--- src/Components/Portfolio/WorkCard.jsx
+++ src/Components/Portfolio/WorkCard.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 function WorkCard({ item }) {
   return (
-    <div className="work__card bg-containerColor p-5 rounded-2xl">
+    <div className="work__card bg-containerColor p-5 rounded-2xl grid gap-1">
       <img src={item.img} alt="" className="work__img w-72 mb-4" />
-      <h3 className="work__title text-base font-medium mb-2">{item.title}</h3>
+      <h3 className="text-base font-medium">{item.title}</h3>
+      <p className="text-xs text-textColor">{item.tools}</p>
       <a
         href={item.url}
-        className="work__button text-textColor text-sm flex items-center gap-x-1 hover"
+        className="work__button text-textColor hover:text-titleDarkColor text-sm flex items-center gap-x-1 hover"
         target="_blank"
       >
         Ver{" "}
-        <i className="bx bx-right-arrow-alt work__button-icon text-base transition-[0.4s]"></i>
+        <i className="bx bx-right-arrow-alt text-base transition-[0.4s]"></i>
       </a>
     </div>
   );
